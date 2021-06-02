59b884f5a420ee76456c50aca04273c11adc4a07
윤주영 <pulpul8282@naver.com>
2021-06-02 16:27:36 +0900

    자유게시판VO

---
+++ src/main/java/org/yoon/domain/BoardVO.java
@@ -0,0 +1,24 @@
+package org.yoon.domain;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class BoardVO {
+	private Long bno; // 글 번호
+	private String title; //제목
+	private String content; // 내용
+	private String writer; //작성자
+	private Date regdate; // 등록 일자
+	private Date updatedate; //수정 일자
+	private int replycnt;// 댓글 수
+	private int visit; // 방문 수
+	private int recomend; //추천 수
+	
+	private List<BoardAttachVO>attachList;
+	
+	
+
+}
