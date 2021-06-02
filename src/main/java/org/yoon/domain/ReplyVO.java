b8e87c4a287a81e5c07be05ae80d69e3825e31c7
윤주영 <pulpul8282@naver.com>
2021-06-02 18:37:26 +0900

    댓글 VO

---
+++ src/main/java/org/yoon/domain/ReplyVO.java
@@ -0,0 +1,16 @@
+package org.yoon.domain;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class ReplyVO {
+		private Long rno; // 댓글 번호
+		private Long bno; // 글 번호
+		
+		private String reply; //댓글 내용
+		private String replayer; //댓글 작성자
+		private Date replyDate; //작성일
+		private Date updateDate; //수정일
+}
