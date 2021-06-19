41e8bca83544ce981c60bf78ba66e5fe8ff2ad49
윤주영 <pulpul8282@naver.com>
2021-06-19 16:06:29 +0900

    [회원 서비스 구현체]

--- src/main/java/org/yoon/service/MemberServiceImpl.java
+++ src/main/java/org/yoon/service/MemberServiceImpl.java
@@ -1,8 +1,15 @@
 package org.yoon.service;
 
-import org.springframework.security.crypto.password.PasswordEncoder;
+import java.util.List;
+
+import org.springframework.security.crypto.password.PasswordEncoder; 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.yoon.domain.BoardAttachVO;
+import org.yoon.domain.GBoardVO;
+import org.yoon.domain.MemberAttachVO;
 import org.yoon.domain.MemberVO;
+import org.yoon.mapper.AttachMapper;
 import org.yoon.mapper.MemberMapper;
 
 import lombok.AllArgsConstructor;
@@ -14,6 +21,7 @@ import lombok.extern.log4j.Log4j;
 public class MemberServiceImpl implements MemberService{
 
 	private MemberMapper mapper;
+	private AttachMapper AttachMapper;
 	
 	private PasswordEncoder pwen;
 	@Override
@@ -22,7 +30,7 @@ public class MemberServiceImpl implements MemberService{
 		return mapper.idChk(userid);
 	}
 
-
+	@Transactional
 	@Override
 	public void insert(MemberVO vo) {
 		log.info("==회원 등록==");
@@ -31,5 +39,42 @@ public class MemberServiceImpl implements MemberService{
 		mapper.insert(vo);
 		
 	}
+
+
+	@Override
+	public MemberVO read(String userid) {
+		log.info("===회원 정보 가져오기==="+userid);
+		return mapper.read(userid);
+	}
+
+	@Transactional
+	@Override
+	public int update(MemberVO vo) {
+		// TODO Auto-generated method stub
+		log.info("==수정된 회원정보==: "+vo);
+		//프로필 변경하기 위해서 기존의 프로필 파일을 먼저 삭제해준다.
+		AttachMapper.deleteUser(vo.getUserid());
+		//변경 적용
+		int cnt = mapper.update(vo);
+		
+		//변경 적용후 변경내용에 프로필 사진이 포함되어 있으면 프로필 내용도 새로 갱신해준다.
+		if(cnt!=0 && vo.getAttachList()!=null && vo.getAttachList().size()>0) {
+			vo.getAttachList().forEach(attach ->{
+				attach.setUserid(vo.getUserid());
+				AttachMapper.insertUser(attach);
+			});
+		}
+		
+		return cnt;
+	}
+
+
+	@Override
+	public List<BoardAttachVO> getAttachList(String userid) {
+		log.info(userid+"회원의 프로필 파일 가져오기");
+		return AttachMapper.findByUser(userid);
+	}
+
+
 
 }
