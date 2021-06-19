d4268537e9108ad76dc775d5917d018bdb944396
윤주영 <pulpul8282@naver.com>
2021-06-19 16:05:39 +0900

    [회원 서비스 인터페이스]

--- src/main/java/org/yoon/service/MemberService.java
+++ src/main/java/org/yoon/service/MemberService.java
@@ -1,6 +1,11 @@
 package org.yoon.service;
 
+import java.util.List;
+
+import org.yoon.domain.BoardAttachVO;
 import org.yoon.domain.BoardVO;
+import org.yoon.domain.GBoardVO;
+import org.yoon.domain.MemberAttachVO;
 import org.yoon.domain.MemberVO;
 
 public interface MemberService {
@@ -10,5 +15,12 @@ public interface MemberService {
 	
 	//회원 등록
 	public void insert(MemberVO vo);
-
+	
+	//회원 정보 가져오기
+	public MemberVO read(String userid);
+	//회원 정보 수정
+	public int update(MemberVO vo);
+	//회원 프로필 사진파일 가져오기
+	public List<BoardAttachVO>getAttachList(String userid);
+	
 }
