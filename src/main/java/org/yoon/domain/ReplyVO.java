fbade0697c996013fa014ba2ce699a704c77cb37
윤주영 <pulpul8282@naver.com>
2021-07-28 21:22:03 +0900

    계층형 쿼리 컬럼 추가

--- src/main/java/org/yoon/domain/ReplyVO.java
+++ src/main/java/org/yoon/domain/ReplyVO.java
@@ -12,9 +12,13 @@ public class ReplyVO {
 		private String replayer; //댓글 작성자
 		private Date replyDate; //작성일
 		private Date updateDate; //수정일
-		private char hide;
+		private char hide;    //댓글 공개/비공개여부
+		private Long parent; //대댓글의 부모 댓글번호 == rno
+		private int deep;  //댓글 깊이
+		private int groupno; // 웹상에서 표현되는 실제 댓글 순서
 		
-		//해당 게시글 타입
+		
+		//해당 게시글 타입=>내가 쓴 댓글목록 확인시 사용됨
 		private String title;
 		private String type;
 		
