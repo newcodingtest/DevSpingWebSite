ccb0c183f125b84449df86a82ee636fc81860560
윤주영 <pulpul8282@naver.com>
2021-07-28 21:46:08 +0900

    +나의 활동 관련 기능 추가

--- src/main/java/org/yoon/mapper/ReplyMapper.java
+++ src/main/java/org/yoon/mapper/ReplyMapper.java
@@ -1,13 +1,16 @@
 package org.yoon.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Param;
+import org.yoon.domain.BoardVO;
 import org.yoon.domain.Criteria;
 import org.yoon.domain.ReplyVO;
 
 public interface ReplyMapper {
-
+	//회원이 쓴 댓글 출력
+	public List<ReplyVO> getListByUser(@Param("cri") Criteria cri,@Param("userid") String userid);
 	//댓글 등록	
 	public int insert(ReplyVO vo);
 	//댓글 조회
@@ -20,5 +23,9 @@ public interface ReplyMapper {
 	public List<ReplyVO> getListPaging(@Param("cri") Criteria cri,@Param("bno") Long bno);
 	//댓글 숫자 파악
 	public int getCountByBno(Long bno);
+	//게시글 삭제에 따른 댓글 삭제
+	public int deleteBno(Long bno);
+	//내가 쓴 댓글 개수 
+	public int getTotalByUser(String userid);
 	
 }
