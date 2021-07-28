9f4281997617febbe978d5f214609cf46fe3e54f
윤주영 <pulpul8282@naver.com>
2021-07-28 21:45:45 +0900

    +나의활동 관련 기능 추가

--- src/main/java/org/yoon/mapper/BoardMapper.java
+++ src/main/java/org/yoon/mapper/BoardMapper.java
@@ -2,6 +2,7 @@ package org.yoon.mapper;
 
 import java.util.HashMap;  
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Param;
 import org.yoon.domain.BoardVO;
@@ -10,7 +11,8 @@ import org.yoon.domain.ReplyVO;
 
 
 public interface BoardMapper {
-	
+	//회원이 쓴 글 출력
+	public List<BoardVO>getListByUser(Map<String, Object> map);
 	//단순 글 출력
 	public List<BoardVO> getList();
 	//글 페이징 출력
@@ -23,10 +25,14 @@ public interface BoardMapper {
 	public BoardVO read(Long bno);
 	//글 삭제
 	public int delete(Long bno);
+	//아이디에 따른 글 삭제
+	public int deleteByUser(String userid);
 	//글 수정
 	public int update(BoardVO board);
 	//페이징에 쓰일 글 전체 개수
 	public int getTotal(Criteria cri);
+	//페이징에 쓰일 유저 가 쓴 전체 개수
+	public int getTotalByUser(String userid);
 	//조회수 증가
 	public int visit(Long bno);
 	//댓글수 증가
@@ -45,14 +51,13 @@ public interface BoardMapper {
 	public List<BoardVO> getNewList();
 	//베스트글 조회
 	public List<BoardVO> getBestList();
-
 	//내가 쓴 글(자유,갤러리) 가져오기
-		public List<BoardVO> getNewboard(String userid);
-		//내가 쓴 글(자유,갤러리) 가져오기
-		public List<ReplyVO> getNewReply(String userid);
-		//내가 쓴 글 개수 출력
-		public int getAllboardCount(String userid);
-		//내가 쓴 댓글 개수 출력
-		public int getAllReplyCount(String userid);
+	public List<BoardVO> getNewboard(String userid);
+	//내가 쓴 글(자유,갤러리) 가져오기
+	public List<ReplyVO> getNewReply(String userid);
+	//내가 쓴 글 개수 출력
+	public int getAllboardCount(String userid);
+	//내가 쓴 댓글 개수 출력
+	public int getAllReplyCount(String userid);
 	
 }
